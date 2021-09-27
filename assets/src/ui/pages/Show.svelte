4331aabdb56ca95af4d9368ec2f4f31fb6106601
Aleksandr <san.crot@yandex.ru>
2021-09-27 17:43:14 +0300

    Render qr using <svg>

--- assets/src/ui/pages/Show.svelte
+++ assets/src/ui/pages/Show.svelte
@@ -1,56 +1,25 @@
 <script lang="ts">
-  import { onMount, onDestroy } from 'svelte';
-
   import { data as datastore } from '../../logic/socket';
   import { qr64 } from '../../logic/utils';
 
   let status: string;
   let data: string | null;
   let qr = qr64();
-  let anim: number;
-  let canvHeight: number = 128;
-  let canv: HTMLCanvasElement;
-  let ctx: CanvasRenderingContext2D;
   $: {
     status = $datastore.status;
     data = $datastore.data;
     qr = qr64(data);
   }
-
-  function drawFrame() {
-    anim = requestAnimationFrame(drawFrame, canv);
-
-    const width = qr.viewBox[2], height = qr.viewBox[3];
-    const dpr = devicePixelRatio || 1;
-    const size = canv.getBoundingClientRect();
-    canvHeight = size.width;
-    canv.width = Math.ceil(size.width * dpr / width) * width;
-    canv.height = Math.ceil(size.height * dpr / height) * height;
-
-    ctx.resetTransform();
-    ctx.scale(canv.width / width, canv.height / height);
-    ctx.translate(-qr.viewBox[0], -qr.viewBox[1]);
-
-    ctx.clearRect(...qr.viewBox);
-    ctx.globalAlpha = data ? 1 : 0.3;
-    ctx.fill(new Path2D(qr.path), "evenodd");
-  }
-
-	onMount(() => {
-    ctx = canv?.getContext("2d");
-    drawFrame();
-	});
-
-  onDestroy(() => {
-    cancelAnimationFrame(anim);
-  });
 </script>
 
 <main>
   <div class="grow"></div>
 
   <div class="qr">
-    <canvas bind:this={canv} style="height: {canvHeight}px"></canvas>
+    <svg viewBox={qr.viewBox} opacity={data ? 1 : 0.3}>
+      <title>{data}</title>
+      <path d={qr.path} fill-rule="evenodd"></path>
+    </svg>
     {#if data === null || data === undefined}
       <span>No data yet</span>
     {/if}
@@ -80,8 +49,9 @@
     margin: 2rem 0;
   }
 
-  .qr > canvas {
+  .qr > svg {
     width: min(100%, 30rem);
+    height: auto;
   }
 
   .qr > span {
