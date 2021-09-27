01bc427ccbb30a78f6282780654fa7b74dba94db
Aleksandr <san.crot@yandex.ru>
2021-09-27 18:36:10 +0300

    Change backend config loader

--- config/runtime.exs
+++ config/runtime.exs
@@ -12,15 +12,21 @@ if config_env() == :prod do
   #   secret_key_base: secret_key_base
 end
 
-hostname = case config_env() do
+origin = URI.parse(case config_env() do
   :prod ->
     System.get_env("HTTP_ORIGIN") ||
       raise "environment variable HTTP_ORIGIN is missing."
-  _ -> System.get_env("HTTP_ORIGIN") || "localhost"
+  _ -> System.get_env("HTTP_ORIGIN") || "//*."
+end)
+
+unless origin.host do
+  raise "Expected an origin with a host that is parsable by URI.parse/1"
 end
 
+IO.puts(:stdio, "HTTP_ORIGIN is " <> inspect(origin))
+
 config :baambook, BaambookWeb.Endpoint,
-  url: [host: hostname],
+  check_origin: [origin],
   http: [
     port: String.to_integer(System.get_env("PORT") || "4000")
   ]
